bf85ff12a197fa7d9e14aa63c17e4d311d53d27a
Enermatz <160655242+Enermatz@users.noreply.github.com>
2024-03-03 09:22:57 +0100

    Update grpc-call.js

--- grpc/grpc-call.js
+++ grpc/grpc-call.js
@@ -1,26 +1,26 @@
 module.exports = function (RED) {
     "use strict";
-    const grpc = require("@grpc/grpc-js");
-    const getByPath = require('lodash.get');
-    const utils = require('../utils/utils');
-    const fs = require("fs");
+    let grpc = require("@grpc/grpc-js");
+    let getByPath = require('lodash.get');
+    let utils = require('../utils/utils');
+    let fs = require("fs");
 
     function gRpcCallNode(config) {
         try {
-            const node = this;
+            var node = this;
             RED.nodes.createNode(node, config);
 
             // Get the gRPC server from the server config Node
-            const serverNode = RED.nodes.getNode(config.server)
+            var serverNode = RED.nodes.getNode(config.server)
             node.on("input", function (msg) {
-                // Overriding config with msg content
+                // overring config with msg content
                 config.service = config.service || msg.service;
                 config.method = config.method || msg.method;
                 
                 try {
                     const REMOTE_SERVER = serverNode.server + ":" + serverNode.port;
-                    // Create gRPC client
-                    let proto =  serverNode.proto;
+                    //Create gRPC client
+                    var proto =  serverNode.proto;
                     if (serverNode.protoPackage) {
                         proto = getByPath(serverNode.proto, serverNode.protoPackage);
                     }
@@ -39,8 +39,8 @@ module.exports = function (RED) {
                                 credentials = grpc.credentials.createSsl();
                             } else if (serverNode.caPath){
                                 if (serverNode.mutualTls){
-                                    const chain = utils.tempFile('cchain.txt', node.chain)
-                                    const key = utils.tempFile('ckey.txt', node.key)
+                                    var chain =  utils.tempFile('cchain.txt', node.chain)
+                                    var key =  utils.tempFile('ckey.txt', node.key)
                         
                                     credentials = grpc.credentials.createSsl(
                                         fs.readFileSync(serverNode.caPath),
@@ -55,83 +55,53 @@ module.exports = function (RED) {
                             }
                         }
                         
-                        const metadata = new grpc.Metadata();
-                        if (typeof msg.metadata === 'object'){
-                            Object.keys(msg.metadata).forEach(key => {
-                                try {
-                                    metadata.add(key, JSON.stringify(msg.metadata[key]));
-                                } catch (e){
-                                    node.error(e);
-                                }
-                            });
-                        }
+                        node.client = new proto[config.service](
+                            REMOTE_SERVER,
+                            credentials || grpc.credentials.createInsecure()
+                        );
+                    }
 
-                        if (node.client) {
-                            if (!node.client[config.method]) {
-                                node.status({fill:"red",shape:"dot",text: "Method " + config.method + " not in proto file"});
-                            } else {
-                                node.status({});
-                                if (proto[config.service].service[config.method].responseStream) {
-                                    node.channel = node.client[config.method](msg.payload, metadata);
-                                    let http2Content = ''; // Accumulate HTTP/2 content
-                                    let headers = {}; // Store response headers
-                                    let trailers = {}; // Store response trailers
-                                    let statusCode = null; // Store response status code
-                                    
-                                    // Event: Data
-                                    node.channel.on("data", function (data) {
-                                        http2Content += data; // Accumulate received data
-                                        // If you want to send each chunk of data separately, send it here
-                                    });
-
-                                    // Event: Headers
-                                    node.channel.on("status", function (status) {
-                                        statusCode = status.code; // Store status code
-                                        headers = status.metadata.getMap(); // Store response headers
-                                    });
-
-                                    // Event: Trailers
-                                    node.channel.on("end", function () {
-                                        // HTTP/2 content received completely, send out the response
-                                        const response = {
-                                            statusCode: statusCode,
-                                            headers: headers,
-                                            trailers: trailers,
-                                            body: http2Content
-                                        };
-                                        msg.payload = response;
-                                        msg.metadata = metadata; // Pass metadata along with the response payload
-                                        node.send(msg);
-                                    });
-
-                                    node.channel.on("error",function (error) {
-                                        msg.error = error;
-                                        node.send(msg);
-                                    });
-
-                                } else {
-                                    node.client[config.method](msg.payload, metadata, function(error, data, responseMetadata) {
-                                        const response = {
-                                            statusCode: responseMetadata.status.code,
-                                            headers: responseMetadata.metadata.getMap(),
-                                            trailers: responseMetadata.trailers,
-                                            body: data
-                                        };
-                                        msg.payload = response;
-                                        msg.metadata = metadata; // Pass metadata along with the response payload
-                                        msg.error = error;
-                                        node.send(msg);
-                                    })
-                                }
+                    let metadata = new grpc.Metadata();
+                    if (typeof msg.metadata === 'object'){
+                        Object.keys(msg.metadata).forEach(key => {
+                            try {
+                                metadata.add(key, JSON.stringify(msg.metadata[key]));
+                            } catch (e){
+                                node.error(e);
                             }
-                        } else {
-                            node.error("gRPC client is not initialized.");
-                        }
+                        });
+                    }
 
+                    if (node.client) {
+                        if (!node.client[config.method]) {
+                            node.status({fill:"red",shape:"dot",text: "Method " + config.method + " not in proto file"});
+                        } else {
+                            node.status({});
+                            if (proto[config.service].service[config.method].responseStream) {
+                                node.channel = node.client[config.method](msg.payload, metadata);
+                                node.channel.on("data", function (data) {
+                                    msg.payload = data;
+                                    node.send(msg);
+                                });
+
+                                node.channel.on("error",function (error) {
+                                    msg.error = error;
+                                    node.send(msg);
+                                });
+
+                            } else {
+                                node.client[config.method](msg.payload, metadata, function(error, data) {
+                                    msg.payload = data;
+                                    msg.error = error;
+                                    node.send(msg);
+                                })
+                            }
+                        }
                     }
 
                 } catch (err) {
-                    node.error("Error occurred: " + err);
+                    node.log("onInput" + err);
+                    console.log(err);
                 }
 
             });
